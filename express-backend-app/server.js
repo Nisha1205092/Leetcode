cf80e660d183c4155e91751bbd3ddfa765435585
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-04-08 01:11:38 +0600

    removed a few comments

--- express-backend-app/server.js
+++ express-backend-app/server.js
@@ -40,14 +40,6 @@ const port = 3000;
 
 
 app.post('/signup', (req, res) => {
-    // Add logic to decode body
-    // body should have email and password
-    // res.send('signup')
-    //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesnt exist)
-    // return back 200 status code to the client
-
-    console.log("req.body", req.body);
-
     const email = req.body.email;
     const password = req.body.password;
 
@@ -72,14 +64,6 @@ app.post('/login', (req, res) => {
     } else {
         return res.status(401).json({ error: 'error loggin in' });
     }
-
-    // Add logic to decode body
-    // body should have email and password
-    // Check if the user with the given email exists in the USERS array
-    // Also ensure that the password is the same
-    // If the password is the same, return back 200 status code to the client
-    // Also send back a token (any random string will do for now)
-    // If the password is not the same, return back 401 status code to the client
 });
 
 app.get('/questions', (req, res) => {
